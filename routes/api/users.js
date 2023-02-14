1587814f7174c8cc3662d45a6dab603088ec41f0
Evgeniia Leonidova <jennufer85@gmail.com>
2023-02-14 11:52:14 +0200

    avatar ready

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { auth, ctrlWrapper } = require("../../middlewares");
+const { auth, upload,ctrlWrapper } = require("../../middlewares");
 const router = express.Router();
 const { users: ctrl } = require("../../controllers/");
 
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
-
+router.patch("/avatars", auth, upload.single("avatar"),ctrlWrapper(ctrl.updateAvatar));
 module.exports = router;
